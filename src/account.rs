23b5e67bcc470e7de272213daa8e3c304ddac391
Alex Rozgo <alex.rozgo@gmail.com>
2021-06-08 01:07:44 -0600

    fund and balance

--- src/account.rs
+++ src/account.rs
@@ -1,23 +1,18 @@
-// use hex_literal::hex;
-// use sc_service::ChainType;
-// use sc_telemetry::TelemetryEndpoints;
-use serde_json::json;
-// use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-use sp_core::crypto::UncheckedInto;
-use sp_core::{sr25519, Pair, Public};
-// use sp_finality_grandpa::AuthorityId as GrandpaId;
+use sp_core::{Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
-use sugarfunge_runtime::{
-    AccountId, AuraConfig, Balance, BalancesConfig, CurrencyId, CurrencyTokenConfig, GenesisConfig,
-    GrandpaConfig, OrmlTokensConfig, Signature, SudoConfig, SystemConfig, TokenSymbol, DOLLARS,
-    WASM_BINARY,
-};
+use sugarfunge_runtime::{AccountId, Signature};
 
-/// Generate a crypto pair from seed.
+/// Generate a crypto public from seed.
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
     TPublic::Pair::from_string(&format!("//{}", seed), None)
         .expect("static values are valid; qed")
         .public()
+}
+
+/// Generate a crypto pair from seed.
+pub fn get_pair_from_seed<TPublic: Pair>(seed: &str) -> TPublic::Pair {
+    TPublic::Pair::from_string(&format!("//{}", seed), None)
+        .expect("static values are valid; qed")
 }
 
 type AccountPublic = <Signature as Verify>::Signer;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Result};
 use rand::prelude::*;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
-#[cfg(feature = "std")]
-#[cfg(feature = "full_crypto")]
 use sp_core::{crypto::Pair, sr25519};
-// use substrate_api_client::{compose_extrinsic, Api, UncheckedExtrinsicV4, XtStatus};
 use sp_keyring::AccountKeyring;
 use sp_runtime::MultiAddress;
 use substrate_api_client::{compose_extrinsic_offline, Api, UncheckedExtrinsicV4, XtStatus};
 use sugarfunge_runtime::{BalancesCall, Call, Header};
-use substrate_api_client::extrinsic::codec::Compact;
-
-use substrate_api_client::{compose_extrinsic};
 
 mod account;
 #[derive(Serialize, Deserialize)]
@@ -33,7 +26,17 @@ struct FundAccountInput {
 }
 
 #[derive(Serialize, Deserialize)]
-struct FundAccountResponse {
+struct FundAccountOutput {
+    amount: u128,
+}
+
+#[derive(Serialize, Deserialize)]
+struct AccountBalanceInput {
+    seed: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct AccountBalanceOutput {
     amount: u128,
 }
 
@@ -84,7 +87,21 @@ async fn fund_account(req: web::Json<FundAccountInput>) -> Result<HttpResponse> 
         .unwrap();
     println!("[+] Transaction got included in block {:?}", blockh);
 
-    Ok(HttpResponse::Ok().json(FundAccountResponse { amount }))
+    Ok(HttpResponse::Ok().json(FundAccountOutput { amount }))
+}
+
+async fn account_balance(req: web::Json<AccountBalanceInput>) -> Result<HttpResponse> {
+    let node: String = "ws://127.0.0.1:9944".into();
+    let from = account::get_pair_from_seed::<sr25519::Pair>(&req.seed);
+    let who = account::get_account_id_from_seed::<sr25519::Public>(&req.seed);
+
+    let api = Api::new(node).map(|api| api.set_signer(from)).unwrap();
+    let mut amount = 0;
+    if let Ok(Some(account_data)) = api.get_account_data(&who) {
+        amount = account_data.free;
+    }
+
+    Ok(HttpResponse::Ok().json(AccountBalanceOutput { amount }))
 }
 
 #[actix_web::main]
@@ -93,6 +110,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .route("/pair", web::post().to(create_pair))
             .route("/fund", web::post().to(fund_account))
+            .route("/balance", web::get().to(account_balance))
     })
     .bind(("127.0.0.1", 4000))?
     .run()
