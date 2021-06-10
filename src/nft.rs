0c24b4f42acfc241e7953b5e8ab54087b76b7173
Alex Rozgo <alex.rozgo@gmail.com>
2021-06-10 17:09:14 -0600

    blocking events in actix block

--- src/nft.rs
+++ src/nft.rs
@@ -53,46 +53,51 @@ pub async fn create_collection(req: web::Json<CreateCollectionInput>) -> Result<
     println!("[+] Composed Extrinsic:\n {:?}\n", xt);
 
     // Send and watch extrinsic until in block
-    let blockh = web::block::<_, _, ()>(move || {
-        Ok(api
+    let collection_id: Option<u64> = web::block::<_, _, ()>(move || {
+        let blockh = api
             .send_extrinsic(xt.hex_encode(), XtStatus::InBlock)
-            .unwrap())
+            .unwrap();
+
+        let mut wait_for_event = true;
+        let mut collection_id = None;
+
+        while wait_for_event {
+            let event_str = events_out.recv().unwrap();
+            let unhex = Vec::from_hex(event_str).unwrap();
+            let mut er_enc = unhex.as_slice();
+            let event_records = Vec::<EventRecord<Event, Hash>>::decode(&mut er_enc);
+            if let Ok(event_records) = event_records {
+                for event_record in &event_records {
+                    match &event_record.event {
+                        Event::sugarfunge_nft(nft_event) => match &nft_event {
+                            sugarfunge_nft::Event::CollectionCreated(
+                                new_collection_id,
+                                account_id,
+                            ) if *account_id == owner_account_id => {
+                                println!("[+] Event: {:?}", nft_event);
+                                collection_id = Some(*new_collection_id);
+                                wait_for_event = false;
+                                break;
+                            }
+                            _ => (),
+                        },
+                        _ => (),
+                    }
+                }
+            }
+            // TODO: break on some threshold
+        }
+
+        println!("[+] Transaction got included in block {:?}", blockh);
+
+        Ok(collection_id)
+
+        // Some(collection_id)
     })
     .await
     .unwrap();
 
-    println!("[+] Transaction got included in block {:?}", blockh);
-
-    let mut collection_id = 0;
-    let mut wait_for_event = true;
-
-    while wait_for_event {
-        let event_str = events_out.recv().unwrap();
-        let _unhex = Vec::from_hex(event_str).unwrap();
-        let mut _er_enc = _unhex.as_slice();
-        let event_records = Vec::<EventRecord<Event, Hash>>::decode(&mut _er_enc);
-        if let Ok(event_records) = event_records {
-            for event_record in &event_records {
-                match &event_record.event {
-                    Event::sugarfunge_nft(nft_event) => match &nft_event {
-                        sugarfunge_nft::Event::CollectionCreated(new_collection_id, account_id)
-                            if *account_id == owner_account_id =>
-                        {
-                            println!("[+] Event: {:?}", nft_event);
-                            collection_id = *new_collection_id;
-                            wait_for_event = false;
-                            break;
-                        }
-                        _ => (),
-                    },
-                    _ => (),
-                }
-            }
-        }
-        // TODO: break on some threshold
-    }
-
     Ok(HttpResponse::Ok().json(CreateCollectionOutput {
-        collection_id: collection_id,
+        collection_id: collection_id.unwrap(),
     }))
 }
